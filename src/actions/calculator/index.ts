d39fab06dcec1e476b398b527cd3c12eec2dd18e
ntrv <6658243+ntrv@users.noreply.github.com>
2018-10-23 03:13:04 +0900

    Fix lint config

--- src/actions/calculator/index.ts
+++ src/actions/calculator/index.ts
@@ -4,10 +4,10 @@ import * as types from './types';
 const actionCreator = actionCreatorFactory();
 
 export const calculatorActions = {
+    clear: actionCreator<void>(types.CLEAR),
+    equal: actionCreator<void>(types.EQUAL),
     inputNumber: actionCreator<number>(types.INPUT_NUMBER),
-    plus: actionCreator<void>(types.PLUS),
     minus: actionCreator<void>(types.MINUS),
     multiple: actionCreator<void>(types.MULTIPLE),
-    equal: actionCreator<void>(types.EQUAL),
-    clear: actionCreator<void>(types.CLEAR),
+    plus: actionCreator<void>(types.PLUS),
 };
